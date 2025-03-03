103b69245a26bf22c7fd483f9679df075515c53b
Vulpes Labs <163655872+vulpeslab@users.noreply.github.com>
2025-03-03 05:40:02 +0100

    Update auth.dbs.js

--- dbs_modules/auth.dbs.js
+++ dbs_modules/auth.dbs.js
@@ -203,7 +203,7 @@ loginButton.addEventListener("click", async (event) => {
 
           if (chowBalConfig === null) {
             localStorage.setItem("initChowBal", "active");
-            localStorage.setItem("usrChowBal", "80000");
+            localStorage.setItem("usrChowBal", "5000");
           } else {
             // leave this for update feature
             console.log("Balance updates from the server is not available!");
